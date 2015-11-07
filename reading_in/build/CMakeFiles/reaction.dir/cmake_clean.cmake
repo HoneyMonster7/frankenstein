48f180210e3c19ec4413076160690fef0a4b7a7d
balint <balint@Ebtelep.lan>
2015-11-07 19:06:34 +0000

    static class variables working, need to restructure cell functions

---
+++ reading_in/build/CMakeFiles/reaction.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reaction.dir/main.cpp.o"
+  "CMakeFiles/reaction.dir/reaction.cpp.o"
+  "CMakeFiles/reaction.dir/cell.cpp.o"
+  "reaction.pdb"
+  "reaction"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reaction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
