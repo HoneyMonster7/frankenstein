d95f63b1f806351c47f521c4a07f41635c84c2b6
Balint Borgulya <s1134965@cplab101.ph.ed.ac.uk>
2015-11-24 14:01:44 +0000

    added script loading network into cytoscape

---
+++ reading_in/cytoscript.sh
@@ -0,0 +1,8 @@
+network load file file=build/initial.xgmml
+vizmap load file file=frankenstyle.xml
+vizmap apply styles=Frankenstyle
+layout apply preferred
+
+network load file file=build/final.xgmml
+vizmap apply styles=Frankenstyle
+layout apply preferred
